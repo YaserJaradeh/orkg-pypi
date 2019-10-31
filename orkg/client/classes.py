673ed757ad3c6f0cb89b5b331a5e3b31eedfccde
Yaser Jaradeh <Yaser.Jaradeh@tib.eu>
2019-10-31 16:49:27 +0100

    implement classes client

---
+++ orkg/client/classes.py
@@ -0,0 +1,53 @@
+from .utils import NamespacedClient, query_params, dict_to_url_params
+
+
+class ClassesClient(NamespacedClient):
+
+    def by_id(self, id):
+        self.client.backend._append_slash = True
+        response = self.client.backend.classes(id).GET()
+        return response.status_code, response.json()
+
+    @query_params("q", "exact")
+    def get(self, params=None):
+        if len(params) > 0:
+            self.client.backend._append_slash = False
+            response = self.client.backend.classes.GET(dict_to_url_params(params))
+        else:
+            self.client.backend._append_slash = True
+            response = self.client.backend.classes.GET()
+        return response.status_code, response.json()
+
+    @query_params("page", "items", "sortBy", "desc")
+    def get_resource_by_class(self, class_name, params=None):
+        if len(params) > 0:
+            self.client.backend._append_slash = False
+            response = self.client.backend.classes(class_name).resources.GET(dict_to_url_params(params))
+        else:
+            self.client.backend._append_slash = True
+            response = self.client.backend.classes(class_name).resources.GET()
+        return response.status_code, response.json()
+
+    @query_params("id", "label", "uri")
+    def add(self, params=None):
+        if len(params) == 0:
+            raise ValueError("at least label should be provided")
+        else:
+            self.client.backend._append_slash = True
+            response = self.client.backend.classes.POST(json=params)
+        return response.status_code, response.json()
+
+    @query_params("label", "uri")
+    def update(self, id, params=None):
+        if len(params) == 0:
+            raise ValueError("at least label should be provided")
+        else:
+            if not self.exists(id):
+                raise ValueError("the provided id is not in the graph")
+            self.client.backend._append_slash = True
+            response = self.client.backend.classes(id).PUT(json=params)
+        return response.status_code, response.json()
+
+    def exists(self, id):
+        return self.by_id(id)[0] == 200
+
