977fabeb8704ecc7b9d2de3c600b5fdd9b111df9
Yaser Jaradeh <Yaser.Jaradeh@tib.eu>
2019-11-01 12:25:30 +0100

    fix tests structure

--- tests/test_predicates.py
+++ tests/test_predicates.py
@@ -2,39 +2,39 @@ from unittest import TestCase
 from orkg import ORKG
 
 
-class TestResources(TestCase):
+class TestPredicates(TestCase):
     """
     Some test scenarios might need to be adjusted to the content of the running ORKG instance
     """
     orkg = ORKG()
 
     def test_by_id(self):
-        res = self.orkg.resources.by_id('R1')
+        res = self.orkg.predicates.by_id('P1')
         self.assertTrue(res.succeeded)
 
     def test_get(self):
-        res = self.orkg.resources.get()
+        res = self.orkg.predicates.get()
         self.assertTrue(res.succeeded)
         self.assertEqual(len(res.content), 10)
 
     def test_get_with_items(self):
         count = 30
-        res = self.orkg.resources.get(items=30)
+        res = self.orkg.predicates.get(items=30)
         self.assertTrue(res.succeeded)
         self.assertEqual(len(res.content), 30)
 
     def test_add(self):
-        label = "test"
-        res = self.orkg.resources.add(label=label)
+        label = "test predicate"
+        res = self.orkg.predicates.add(label=label)
         self.assertTrue(res.succeeded)
         self.assertEqual(res.content['label'], label)
 
     def test_update(self):
-        res = self.orkg.resources.add(label="Coco")
+        res = self.orkg.predicates.add(label="Coco predicate")
         self.assertTrue(res.succeeded)
-        label = "test"
-        res = self.orkg.resources.update(id=res.content['id'], label=label)
+        label = "Test predicate"
+        res = self.orkg.predicates.update(id=res.content['id'], label=label)
         self.assertTrue(res.succeeded)
-        res = self.orkg.resources.by_id(res.content['id'])
+        res = self.orkg.predicates.by_id(res.content['id'])
         self.assertTrue(res.succeeded)
         self.assertEqual(res.content['label'], label)
