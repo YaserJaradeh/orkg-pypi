de4e8723fdcb5558e72deb48ce11441ea770af09
Yaser Jaradeh <Yaser.Jaradeh@tib.eu>
2019-11-01 11:41:22 +0100

    unit test resources

--- orkg/client/backend.py
+++ orkg/client/backend.py
@@ -13,7 +13,7 @@ class ORKG(object):
         if host is not None and not host.startswith('http'):
             raise ValueError('host must begin with http or https')
         self.host = host if host is not None else 'http://127.0.0.1:8000'
-        self.backend: hammock.Hammock = hammock.Hammock(self.host).api
+        self.backend = hammock.Hammock(self.host).api
         self.resources = ResourcesClient(self)
         self.predicates = PredicatesClient(self)
         self.classes = ClassesClient(self)
--- tests/test_resources.py
+++ tests/test_resources.py
@@ -14,3 +14,10 @@ class TestResources(TestCase):
         res = orkg.resources.get()
         self.assertTrue(res.succeeded)
         self.assertEqual(len(res.content), 10)
+
+    def test_get_with_items(self):
+        orkg = ORKG()
+        count = 30
+        res = orkg.resources.get(items=30)
+        self.assertTrue(res.succeeded)
+        self.assertEqual(len(res.content), 30)
