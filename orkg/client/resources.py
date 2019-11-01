72adc2a57a1fc62523a1e34ceae90da849bf19e0
Yaser Jaradeh <Yaser.Jaradeh@tib.eu>
2019-11-01 11:41:03 +0100

    document functions

--- orkg/client/resources.py
+++ orkg/client/resources.py
@@ -5,12 +5,28 @@ from orkg.out import OrkgResponse
 class ResourcesClient(NamespacedClient):
 
     def by_id(self, id):
+        """
+        Lookup a resource by id
+        :param id: the id of the resource to lookup
+        :return: an OrkgResponse object containing the resource
+        """
         self.client.backend._append_slash = True
         response = self.client.backend.resources(id).GET()
         return OrkgResponse(response)
 
     @query_params("q", "exact", "page", "items", "sortBy", "exclude", "desc")
     def get(self, params=None):
+        """
+        Fetch a list of resources, with the possibility to paginate the results and filter them out based on label
+        :param q: search term of the label of the resource (optional)
+        :param exact: whether to check for the exact search term or not (optional) -> bool
+        :param page: the page number (optional)
+        :param items: number of items per page (optional)
+        :param sortBy: key to sort on (optional)
+        :param desc: true/false to sort desc (optional)
+        :param exclude: classes to be excluded in search (optional)
+        :return: an OrkgResponse object contains the list of resources
+        """
         if len(params) > 0:
             self.client.backend._append_slash = False
             response = self.client.backend.resources.GET(dict_to_url_params(params))
@@ -21,6 +37,13 @@ class ResourcesClient(NamespacedClient):
 
     @query_params("id", "label", "classes")
     def add(self, params=None):
+        """
+        Create a new resource in the ORKG instance
+        :param id: the specific id to add (optional)
+        :param label: the label of the new resource (optional)
+        :param classes: list of classes to assign the resource to (optional)
+        :return: an OrkgResponse object containing the newly created resource
+        """
         if len(params) == 0:
             raise ValueError("at least label should be provided")
         else:
@@ -30,6 +53,13 @@ class ResourcesClient(NamespacedClient):
 
     @query_params("label", "classes")
     def update(self, id, params=None):
+        """
+        Update a resource with a specific id
+        :param id: the id of the resource to update
+        :param label: the new label (optional)
+        :param classes: the updated list of classes (optional)
+        :return: an OrkgResponse object contains the newly updated resource
+        """
         if len(params) == 0:
             raise ValueError("at least label should be provided")
         else:
@@ -40,5 +70,10 @@ class ResourcesClient(NamespacedClient):
         return OrkgResponse(response)
 
     def exists(self, id):
+        """
+        Check if a resource exists in the graph
+        :param id: the id of the resource to check
+        :return: true if found, otherwise false
+        """
         return self.by_id(id)[0] == 200
 
